3a7f3033f5e9180a100d4aafd6681efe5ff006b4
Kangsik Kevin Lee <kangsik-kevin-lee@Kangsiks-MacBook-Pro.local>
2024-03-18 10:41:20 +0900

    feat: add validation for passing button type

--- src/Button.js
+++ src/Button.js
@@ -11,4 +11,22 @@ function Button({
   return <button>{children}</button>;
 }
 
+// validation for button types
+Button.propTypes = {
+  checkVariationValue: ({ primary, secondary, success, warning, danger }) => {
+    const count =
+      Number(!!primary) +
+      Number(!!secondary) +
+      Number(!!warning) +
+      Number(!!success) +
+      Number(!!danger);
+
+    if (count > 1) {
+      return new Error(
+        "Only one of primary, secondary, success, warning, danger can be used"
+      );
+    }
+  },
+};
+
 export default Button;
