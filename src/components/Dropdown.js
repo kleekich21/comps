9168fec3a48406d69dd8893aca8c526be8c345d7
Kangsik Kevin Lee <kangsik-kevin-lee@Kangsiks-MacBook-Pro.local>
2024-03-26 17:20:18 +0900

    refactor: use convention for controlled component

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
+import { GoChevronDown } from "react-icons/go";
 
-function Dropdown({ options, selection, onSelect }) {
+function Dropdown({ options, value, onChange }) {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleClick = () => {
@@ -9,22 +10,35 @@ function Dropdown({ options, selection, onSelect }) {
 
   const handleOptionClick = (option) => {
     setIsOpen(false);
-    console.log(option);
-    onSelect(option);
+    onChange(option);
   };
 
   const renderedOptions = options.map((option) => {
     return (
-      <div onClick={() => handleOptionClick(option)} key={option.value}>
+      <div
+        className="hover:bg-sky-100 rounded cursor-pointer p-1"
+        onClick={() => handleOptionClick(option)}
+        key={option.value}
+      >
         {option.label}
       </div>
     );
   });
 
   return (
-    <div>
-      <div onClick={handleClick}>{selection?.label || "Select..."}</div>
-      {isOpen && <div>{renderedOptions}</div>}
+    <div className="w-48 relative">
+      <div
+        className="flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full"
+        onClick={handleClick}
+      >
+        {value?.label || "Select..."}
+        <GoChevronDown className="text-lg" />
+      </div>
+      {isOpen && (
+        <div className="absolute top-full border rounded p-3 shadow bg-white w-full">
+          {renderedOptions}
+        </div>
+      )}
     </div>
   );
 }
