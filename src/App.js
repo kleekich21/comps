87a20f961c46f3baaaf476ec34817e7d17d61258
Kangsik Kevin Lee <kangsik-kevin-lee@Kangsiks-MacBook-Pro.local>
2024-03-26 21:33:43 +0900

    feat: navigation context created, back/forward button on browser is handled

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import { useState } from "react";
-import Dropdown from "./components/Dropdown";
+import { NavigationProvider } from "./context/navigation";
+
 function App() {
-  const [selection, setSelection] = useState(null);
-
-  const handleSelect = (option) => {
-    setSelection(option);
-  };
-
-  const options = [
-    { label: "Red", value: "red" },
-    { label: "Green", value: "green" },
-    { label: "Blue", value: "blue" },
-  ];
-
   return (
-    <div className="flex">
-      <Dropdown options={options} value={selection} onChange={handleSelect} />
-      <Dropdown options={options} value={selection} onChange={handleSelect} />
-    </div>
+    <NavigationProvider>
+      <div></div>
+    </NavigationProvider>
   );
 }
 
