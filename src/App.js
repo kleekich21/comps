03de786486c32004215e3fc47cce493a39766acd
Kangsik Kevin Lee <kangsik-kevin-lee@Kangsiks-MacBook-Pro.local>
2024-03-26 16:45:37 +0900

    feat: dropdown component select

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import Accordion from "./components/Accordion";
-
+import { useState } from "react";
+import Dropdown from "./components/Dropdown";
 function App() {
-  const items = [
-    {
-      id: "asdfadfs",
-      label: "Can I use React on a project?",
-      content: "You can use React on any project you want.",
-    },
-    {
-      id: "asdfadfs2",
-      label: "Can I use Javascript on a project?",
-      content: "You can use React on any project you want.",
-    },
-    {
-      id: "asdfadfs3",
-      label: "Can I use CSS on a project?",
-      content: "You can use React on any project you want.",
-    },
+  const [selection, setSelection] = useState(null);
+
+  const handleSelect = (option) => {
+    setSelection(option);
+  };
+
+  const options = [
+    { label: "Red", value: "red" },
+    { label: "Green", value: "green" },
+    { label: "Blue", value: "blue" },
   ];
-  return <Accordion items={items} />;
+
+  return (
+    <Dropdown options={options} selection={selection} onSelect={handleSelect} />
+  );
 }
 
 export default App;
