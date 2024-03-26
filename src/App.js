2516141b280db50eb51d1a3b48022b9af481cd12
Kangsik Kevin Lee <kangsik-kevin-lee@Kangsiks-MacBook-Pro.local>
2024-03-26 21:54:02 +0900

    feat: navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { NavigationProvider } from "./context/navigation";
+import Link from "./components/Link";
 
 function App() {
   return (
     <NavigationProvider>
-      <div></div>
+      <div>
+        <Link to="/accordion">Go to accordion</Link>
+        <Link to="/dropdown">Go to dropdown</Link>
+      </div>
     </NavigationProvider>
   );
 }
