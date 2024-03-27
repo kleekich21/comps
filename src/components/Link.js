1f530d101810b4ff6729397d3201cf8614c3baac
Kangsik Kevin Lee <kangsik-kevin-lee@Kangsiks-MacBook-Pro.local>
2024-03-27 09:51:52 +0900

    feat: Allow open in a new tab

--- src/components/Link.js
+++ src/components/Link.js
@@ -4,11 +4,18 @@ import NavigationContext from "../context/navigation";
 function Link({ to, children }) {
   const { navigate } = useContext(NavigationContext);
   const handleClick = (event) => {
+    if (event.metaKey || event.ctrlKey) {
+      return;
+    }
     event.preventDefault();
 
     navigate(to);
   };
-  return <a onClick={handleClick}>{children}</a>;
+  return (
+    <a href={to} onClick={handleClick}>
+      {children}
+    </a>
+  );
 }
 
 export default Link;
