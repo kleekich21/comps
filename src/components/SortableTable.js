d9a8b650992a28669015a23b2df317f898e7ec67
Kangsik Kevin Lee <kangsik-kevin-lee@Kangsiks-MacBook-Pro.local>
2024-05-12 19:29:14 +0900

    feat: add icons for sortable header and clicking different header always sorts in ascending order

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { GoChevronDown, GoChevronUp } from "react-icons/go";
 import Table from "./Table";
 
 function SortableTable(props) {
@@ -7,6 +8,12 @@ function SortableTable(props) {
   const { config, data } = props;
 
   const handleClick = (label) => {
+    if (sortBy && label !== sortBy) {
+      setSortOrder("asc");
+      setSortBy(label);
+      return;
+    }
+
     if (sortOrder === null) {
       setSortOrder("asc");
       setSortBy(label);
@@ -26,8 +33,14 @@ function SortableTable(props) {
     return {
       ...column,
       header: () => (
-        <th onClick={() => handleClick(column.label)}>
-          {column.label} IS SORTABLE
+        <th
+          className="cursor-pointer hover:bg-gray-100"
+          onClick={() => handleClick(column.label)}
+        >
+          <div className="flex items-center">
+            {getIcons(column.label, sortBy, sortOrder)}
+            {column.label}
+          </div>
         </th>
       ),
     };
@@ -52,12 +65,39 @@ function SortableTable(props) {
     });
   }
 
-  return (
-    <div>
-      {sortOrder} - {sortBy}
-      <Table {...props} data={sortedData} config={updatedConfig} />
-    </div>
-  );
+  return <Table {...props} data={sortedData} config={updatedConfig} />;
+}
+
+function getIcons(label, sortBy, sortOrder) {
+  if (label !== sortBy) {
+    return (
+      <div>
+        <GoChevronUp />
+        <GoChevronDown />
+      </div>
+    );
+  } else {
+    if (sortOrder === null) {
+      return (
+        <div>
+          <GoChevronUp />
+          <GoChevronDown />
+        </div>
+      );
+    } else if (sortOrder === "asc") {
+      return (
+        <div>
+          <GoChevronUp />
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <GoChevronDown />
+        </div>
+      );
+    }
+  }
 }
 
 export default SortableTable;
