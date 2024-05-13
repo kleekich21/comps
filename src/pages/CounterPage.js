6dedc63611099af8c3fa3ecc6c82d47ccfeed5d1
kleekich21 <kangsik.kevin.lee@gmail.com>
2024-05-13 15:08:00 +0900

    refactor: functionality done

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -4,6 +4,7 @@ import Panel from "../components/Panel";
 
 const CounterPage = ({ initialCount }) => {
   const [count, setCount] = useState(initialCount);
+  const [valueToAdd, setValueToAdd] = useState(0);
 
   const increment = () => {
     setCount(count + 1);
@@ -13,6 +14,19 @@ const CounterPage = ({ initialCount }) => {
     setCount(count - 1);
   };
 
+  const handleChange = (event) => {
+    const value = parseInt(event.target.value) || 0;
+
+    setValueToAdd(value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    setCount(count + valueToAdd);
+    setValueToAdd(0);
+  };
+
   return (
     <Panel lcassName="m-3">
       <h1 className="text-lg">Count is {count}</h1>
@@ -20,9 +34,11 @@ const CounterPage = ({ initialCount }) => {
         <Button onClick={increment}>Increment</Button>
         <Button onClick={decrement}>Decrement</Button>
       </div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Add a lot!</label>
         <input
+          value={valueToAdd || ""}
+          onChange={handleChange}
           type="number"
           className="p-1 m-3 bg-gray-50 border border-gray-500"
         />
