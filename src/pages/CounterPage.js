ba9b419166507fd0938f8f4c5de0b1102c0e7120
kleekich21 <kangsik.kevin.lee@gmail.com>
2024-05-13 16:34:56 +0900

    refactor: change reducer with immer library

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,3 +1,4 @@
+import { produce } from "immer";
 import { useReducer } from "react";
 import Button from "../components/Button";
 import Panel from "../components/Panel";
@@ -10,33 +11,25 @@ const ADD_VALUE_TO_COUNT = "add_value_to_count";
 const reducer = (state, action) => {
   switch (action.type) {
     case INCREMENT_COUNT:
-      return {
-        ...state,
-        count: state.count + 1,
-      };
+      state.count = state.count + 1;
+      return;
     case DECREMENT_COUNT:
-      return {
-        ...state,
-        count: state.count - 1,
-      };
+      state.count = state.count - 1;
+      return;
     case SET_VALUE_TO_ADD:
-      return {
-        ...state,
-        valueToAdd: action.payload,
-      };
+      state.valueToAdd = action.payload;
+      return;
     case ADD_VALUE_TO_COUNT:
-      return {
-        ...state,
-        count: state.count + action.payload,
-        valueToAdd: 0,
-      };
+      state.count = state.count + action.payload;
+      state.valueToAdd = 0;
+      return;
     default:
-      return state;
+      return;
   }
 };
 
 const CounterPage = ({ initialCount }) => {
-  const [state, dispatch] = useReducer(reducer, {
+  const [state, dispatch] = useReducer(produce(reducer), {
     count: initialCount,
     valueToAdd: 0,
   });
@@ -63,6 +56,7 @@ const CounterPage = ({ initialCount }) => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+
     dispatch({
       type: ADD_VALUE_TO_COUNT,
       payload: state.valueToAdd,
